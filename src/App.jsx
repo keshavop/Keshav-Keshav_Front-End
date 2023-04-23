0089dcfaf75bebc9379a2ebbbe6c60d1b7cb4da2
Keshav Kumar <keshavkumar8122@gmail.com>
2023-04-23 19:24:16 +0530

    app

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import WrappedSingleListItem from "./components/WrappedSingleListItem";
+import "./App.css";
+
+function App() {
+  const items = [
+    { text: "Ant" },
+    { text: "Dog" },
+    { text: "Cow" },
+    { text: "Cat" },
+  ];
+
+  return (
+    <>
+        <h1>Steel Eye Assignemnt</h1>
+        <WrappedSingleListItem items={items}></WrappedSingleListItem>
+        <h3>Keshav</h3>
+        <h3>
+          GitHub Link
+          <a
+            href="https://github.com/keshavop/Front-end-Assignment"
+            target="_blank"
+          >
+            Github
+          </a>
+        </h3>
+        <h3>
+          Deployed Link
+          <a href="" target="_balnk">
+            Vercel
+          </a>
+        </h3>
+    </>
+  );
+}
+
+export default App;
